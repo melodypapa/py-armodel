7486a6d9bd834b741cd9fcabcc767a3ac9afb625
melodypapa <melodypapa@outlook.com>
2024-11-16 21:44:35 +0800

    Feature #43: Update the version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='armodel',
-    version='1.6.2',
+    version='1.6.3',
     description='the python arxml parser',
 
     url='http://github.com/melodypapa/py-armodel',
